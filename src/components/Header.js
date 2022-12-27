106ad63a0bf776882c0051cdf1861f91c8e6c3dd
naga <yuto7687@gmail.com>
2022-12-28 03:53:32 +0900

    ✨ 上部headerの作成とfakestoreapiの使用

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
+import Image from "next/image";
 
 function Header() {
   return (
     <header>
+        {/* 上部のナビゲーション */}
+        <div className="flex item-center bg-amazon_blue p-1 flex-grow py-2">
+            <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+                <Image
+                    src="https://links.papareact.com/f90"
+                    width={150}
+                    height={40}
+                    objectFit="contain"
+                    className="cursor-pointer"
+                />
+            </div>
+            
+        </div>
+        {/* 下部のナビゲーション */}
+        <div>
+
+        </div>
     </header>
   )
 }
