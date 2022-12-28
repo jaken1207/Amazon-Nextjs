4e74dc85abb57891e3f0af38a77c4d5ebb9dfe9b
naga <yuto7687@gmail.com>
2022-12-29 07:58:15 +0900

    🐛 検索窓が中央に揃ってなかったので揃えた

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
   return (
     <header>
         {/* 上部のナビゲーション */}
-        <div className="flex item-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
             {/* 左のAmazonロゴ */}
             <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                 <Image
@@ -21,7 +21,7 @@ function Header() {
                 />
             </div>
             {/* 検索窓 */}
-            <div className="hidden sm:flex items-center h10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+            <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
                 <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
                 <SearchIcon className="h-12 p-4"/>
             </div>
