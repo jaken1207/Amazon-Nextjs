26e9265ba3e110cd4e8f2fb0c511100c964dec3f
naga <yuto7687@gmail.com>
2022-12-29 07:59:20 +0900

    ✨ 右のメニューバーを作成しました。

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,6 +25,23 @@ function Header() {
                 <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
                 <SearchIcon className="h-12 p-4"/>
             </div>
+            {/* 右のメニューバー　*/}
+            <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+                <div className="link">
+                    <p>Hello Yuto Nagata</p>
+                    <p className="font-extrabold md:text-sm">Account & Lists</p>
+                </div>
+                <div className="link">
+                    <p>Returns</p>
+                    <p className="font-extrabold md:text-sm">& Orders</p>
+                </div>
+                <div className="relative link flex items-center">
+                    <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black
+                    ">0</span>
+                    <ShoppingCartIcon className="h-10"/>
+                    <p className="hidden sm:inline font-extrabold md:text-sm mt-2">Basket</p>
+                </div>
+            </div>
             
         </div>
         {/* 下部のナビゲーション */}
